5f75dac99af16a55631fa5478f747fbd24c862e5
happkiller1010101 <limfangyijustin@gmail.com>
2023-11-24 21:15:18 +0800

    Implemented the back-end for view-retirement-plan

--- models/Retirement.js
+++ models/Retirement.js
@@ -1,5 +1,5 @@
 class Retirement {
-  constructor(current_age, retirement_age, fund_goal, id) {
+  constructor(current_age, retirement_age, fund_goal) {
     this.current_age = current_age;
     this.retirement_age = retirement_age;
     this.fund_goal = fund_goal;
