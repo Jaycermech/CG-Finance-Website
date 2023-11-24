5d1b1c637db7a037d7f3e0db8afbec2d3d685950
Terriyakiii <Bryanleelee0@gmail.com>
2023-11-24 15:51:36 +0800

    implemented Add Monthly-budget

--- utils/monthly-budgetUtil.js
+++ utils/monthly-budgetUtil.js
@@ -1,39 +1,41 @@
-// const { readJSON, writeJSON } = require("./monthly-budget.json");
 
-const { Monthly_budget } = require("../models/monthly-budget");
 const fs = require("fs").promises;
 
-async function readJSON(filePath) {
+async function readJSON(filename) {
     try {
-      const data = await fs.readFile(filePath, "utf8");
+      const data = await fs.readFile(filename, "utf8");
       return JSON.parse(data);
-    } catch (error) {
-      console.error(error);
+    } catch (err) {
+      console.error(err);
+      throw err;
     }
   }
-async function writeJSON(data, filePath) {
+  async function writeJSON(object, filename) {
     try {
-      await fs.writeFile(filePath, JSON.stringify(data), { encoding: "utf8" });
-    } catch (error) {
-      console.error(error);
+      const allObjects = await readJSON(filename);
+      allObjects.push(object);
+      await fs.writeFile(filename, JSON.stringify(allObjects), "utf8");
+      return allObjects;
+    } catch (err) {
+      console.error(err);
+      throw err;
     }
   }
 
+const { Monthly_budget } = require("../models/monthly-budget");
+
 async function addBudget(req, res) {
-    try {
-        const ammenities = req.body.ammenities;
-        const cost = req.body.cost;
-        const newBudget = new Monthly_budget(ammenities,cost);
-        const updatedBudgets = await writeJSON(
-            newBudget,
-            "utils/monthly-budget.json"
-        );
-        return res.status(201).json(updatedBudgets);
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
-    }
+  try {
+    const ammenities = req.body.ammenities;
+    const cost = req.body.cost;
+    const newBudget = new Monthly_budget(ammenities, cost);
+    const updatedBudgets = await writeJSON(newBudget,"utils/monthly-budget.json");
+    return res.status(201).json(updatedBudgets);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 }
 
 module.exports = {
-    addBudget,
+  addBudget,
 };
