3ccd63442e41923366225a58bec53c44931dfedf
happkiller1010101 <limfangyijustin@gmail.com>
2023-11-24 21:08:15 +0800

    Implemented the back-end for view retirement

--- models/Retirement.js
+++ models/Retirement.js
@@ -1,8 +1,12 @@
 class Retirement {
-    constructor(current_age, retirement_age, fund_goal) { 
+  constructor(current_age, retirement_age, fund_goal, id) {
     this.current_age = current_age;
     this.retirement_age = retirement_age;
     this.fund_goal = fund_goal;
-    } 
-   } 
-   module.exports = { Retirement }; 
+
+    var timestamp = new Date().getTime();
+    var random = Math.floor(Math.random() * 1000);
+    this.id = timestamp + "" + random.toString().padStart(3, "0");
+  }
+}
+module.exports = { Retirement };
