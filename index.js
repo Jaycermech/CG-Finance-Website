d8240a03c40872f5f29e760c2843ee0e4a905d7c
itsukir0 <kennethkhor121@gmail.com>
2023-11-25 13:25:06 +0800

    all back-end working

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+
 const PORT = process.env.PORT || 5050;
 var startPage = "index.html";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+//add retirement
+const { add_retirement } = require("./utils/RetirementUtil");
+app.post("/add-retirement", add_retirement);
+
+//retrieve retirement
+const { view_retirement } = require("./utils/RetirementUtil");
+app.get("/view-retirement", view_retirement);
+
+//update retirement
+const { edit_retirement } = require("./utils/RetirementUtil");
+app.put("/edit-retirement/:id", edit_retirement);
+
+//delete retirement
+const { delete_retirement } = require("./utils/RetirementUtil");
+app.delete("/delete-retirement/:id", delete_retirement);
+
+
+
+// Monthly-budget
+const { addBudget,viewBudget,editBudget,deleteBudget} = require("./utils/monthly-budgetUtils");
+app.post("/add-budget", addBudget);
+app.get("/view-budget", viewBudget);
+app.put("/edit-budget/:id", editBudget);
+app.delete("/delete-budget/:id", deleteBudget);
 
 const {register} = require ("./utils/UserUtil");
 app.post("/register", register);
