d60dc0e2b234cec6b6c2c1a9c63558c295935796
happkiller1010101 <limfangyijustin@gmail.com>
2023-11-25 11:47:20 +0800

    Implemented the backend CRUD for retirement-plan

--- models/Retirement.js
+++ models/Retirement.js
@@ -3,6 +3,7 @@ class Retirement {
     this.current_age = current_age;
     this.retirement_age = retirement_age;
     this.fund_goal = fund_goal;
+
     var timestamp = new Date().getTime();
     var random = Math.floor(Math.random() * 1000);
     this.id = timestamp + "" + random.toString().padStart(3, "0");
