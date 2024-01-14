9bf8bdeb8b4ea0b9377f835c070536bad3f74286
Jaycermech <148203743+Jaycermech@users.noreply.github.com>
2024-01-14 16:02:06 +0800

    front end for expenses done left UI testing

--- public/js/expense.js
+++ public/js/expense.js
@@ -64,9 +64,58 @@ function viewExpenses() {
   request.send();
 }
 
+// function editCategory(category) {
+//   // Implement the logic to handle the edit action for the specified category
+//   console.log("Editing category: " + category);
+//   document.getElementById("categories").textContent = category;
+
+//   function viewResources2() {
+//     var response = "";
+//     var request = new XMLHttpRequest();
+//     request.open("GET", "/view-expneses", true);
+//     request.setRequestHeader("Content-Type", "application/json");
+//     request.onload = function () {
+//       response = JSON.parse(request.responseText);
+//       console.log(response);
+
+//       for (let i = 0; i < response.length; i++) {
+//         if (response[i].description == category) {
+//           console.log(response[i].description);
+//           var html = `
+//           <div class="text-center" style="width:100%;">
+//               <div class="card">
+//                   <div class="card-body">
+//                       <h6 style="color: black;" class="card-text">${response[i].description}</h6>
+//                       <small style="color: black;">Amount: ${response[i].amount}</small>
+//                       <i class='far fa-edit fa-2x edit' description=${response[i].description} amount=${response[i].amount} id=${response[i].id} style="color: black; font-size: 18px; cursor: pointer;" data-toggle='modal' data-target='#editSpecificCategoryModal' data-dismiss='modal' item='${i}' onClick='displaySpecificCategory(this)'></i>
+//                       <i class='fas fa-trash-alt fa-2x delete-btn' data-id='${response[i].id}' data-description='${response[i].description}' onclick='deleteExpenseItem(this)' style="color: red; font-size: 18px; cursor: pointer;"></i>
+//                   </div>
+//               </div>
+//           </div>
+//       `;
+
+//           document
+//             .getElementById("categories")
+//             .insertAdjacentHTML("beforeend", html);
+//         }
+//       }
+//     };
+//     request.send();
+//   }
+//   viewResources2();
+// }
+
 function editCategory(category) {
   // Implement the logic to handle the edit action for the specified category
   console.log("Editing category: " + category);
+
+  // Check if the Useremail in session storage exists
+  var userEmailInSession = sessionStorage.getItem("Useremail");
+  if (!userEmailInSession) {
+    console.log("User email not found in session storage");
+    return;
+  }
+
   document.getElementById("categories").textContent = category;
 
   function viewResources2() {
@@ -79,7 +128,11 @@ function editCategory(category) {
       console.log(response);
 
       for (let i = 0; i < response.length; i++) {
-        if (response[i].description == category) {
+        // Check if Useremail in session matches the user in the data
+        if (
+          response[i].description == category &&
+          response[i].user === userEmailInSession
+        ) {
           console.log(response[i].description);
           var html = `
           <div class="text-center" style="width:100%;">
