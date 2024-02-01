6285bced2fb4e338e3883bcabea4643449317bc9
Jaycermech <148203743+Jaycermech@users.noreply.github.com>
2024-02-01 16:06:22 +0800

    monitoring done

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-
+const logger = require("./logger");
 const PORT = process.env.PORT || 5050;
 var startPage = "index.html";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const statusMonitor = require("express-status-monitor");
+app.use(statusMonitor());
 
 //add retirement
 const { add_retirement } = require("./utils/RetirementUtil");
@@ -83,5 +86,7 @@ app.get("/home", (req, res) => {
 });
 const server = app.listen(PORT, function () {
   console.log(`Demo project at: http://localhost:${PORT}`);
+  logger.info(`Demo project at: http://localhost:${PORT}/status`);
+  logger.error(`Example or error log`);
 });
 module.exports = { app, server };
