f7ca5edc54e2da0ad8911b296a54167c7f46b214
Terriyakiii <148203865+Terriyakiii@users.noreply.github.com>
2024-02-01 16:32:34 +0800

    Completed the implementation of real-time monitoring and logging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-
+const logger = require('./logger');
 const PORT = process.env.PORT || 5050;
 var startPage = "index.html";
 
@@ -81,7 +81,11 @@ app.get("/home", (req, res) => {
 app.get("/home", (req, res) => {
   res.sendFile(__dirname + "/public/" + startPage);
 });
+const statusMonitor = require('express-status-monitor');
+app.use(statusMonitor())
 const server = app.listen(PORT, function () {
   console.log(`Demo project at: http://localhost:${PORT}`);
+  logger.info(`Demo project at: ${PORT}!`);
+  logger.error(`Example or error log`)
 });
 module.exports = { app, server };
